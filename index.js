1abf8bd99dd1aaaff61568949093e5781e4b7781
Adam1401 <128369313+Adam1401@users.noreply.github.com>
2023-10-18 18:57:39 +0200

    premier commit
    première version

---
+++ index.js
@@ -0,0 +1,96 @@
+const canvas = document.getElementById("drawingCanvas");
+const context = canvas.getContext("2d");
+const clearButton = document.getElementById("clearButton");
+const colorPicker = document.getElementById("colorPicker");
+const colorPickerImage = document.getElementById("colorPickerImage");
+let isDrawing = false;
+const undoStack = [];
+const redoStack = [];
+
+// Appeler la fonction de redimensionnement lors du chargement initial de la page
+resizeCanvas();
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = document.documentElement.clientHeight * 0.93;
+}
+
+// Appeler la fonction de redimensionnement lors du redimensionnement de la fenêtre
+window.addEventListener('resize', resizeCanvas);
+
+canvas.addEventListener("mousedown", () => {
+    isDrawing = true;
+});
+
+// Gestionnaire d'événements pour mettre à jour la pile undo lors du dessin
+canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+    context.beginPath();
+    undoStack.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    //redoStack.length = 0; // Réinitialiser la pile redo lorsque de nouveaux dessins sont effectués
+});
+
+canvas.addEventListener("mousemove", draw);
+
+clearButton.addEventListener("click", () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    undoStack.length = 0;
+    redoStack.length = 0;
+});
+
+colorPicker.addEventListener("input", () => {
+    context.strokeStyle = colorPicker.value;
+});
+
+colorPickerImage.addEventListener("click", () => {
+    colorPicker.click();
+});
+
+function draw(event) {
+    if (!isDrawing) return;
+    context.lineWidth = 4;
+    context.lineCap = "round";
+    context.strokeStyle = colorPicker.value;
+
+    context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Delete" || event.key === "Backspace") {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        undoStack.length = 0;
+        redoStack.length = 0;
+    }
+});
+
+// Créer une pile pour stocker les états du dessin
+
+
+// Gestionnaire d'événements pour la touche "Ctrl + Z" (Annuler)
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.key === "z") {
+        event.preventDefault(); // Empêche la navigation arrière du navigateur
+
+        if (undoStack.length > 0) {
+            redoStack.push(context.getImageData(0, 0, canvas.width, canvas.height));
+            context.putImageData(undoStack.pop(), 0, 0);
+        }
+    }
+});
+
+// Gestionnaire d'événements pour la touche "Ctrl + Y" (Rétablir)
+document.addEventListener("keydown", function(event) {
+    if (event.ctrlKey && event.key === "y") {
+        event.preventDefault(); // Empêche la navigation avant du navigateur
+
+        if (redoStack.length > 0) {
+            undoStack.push(context.getImageData(0, 0, canvas.width, canvas.height));
+            context.putImageData(redoStack.pop(), 0, 0);
+        }
+    }
+});
+
+
